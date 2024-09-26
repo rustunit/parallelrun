4cf435fb411f2f789323b0ef2af089979c17e683
extrawurst <mail@rusticorn.com>
2024-09-26 12:27:42 +0300

    make test more ci stable

--- tests/simple.rs
+++ tests/simple.rs
@@ -3,14 +3,14 @@ use std::process::Command;
 #[test]
 fn test_simple() {
     let output = Command::new("target/debug/parallelrun")
-        .args(vec!["echo 1", "sleep 1 && echo 2"])
+        .args(vec!["echo 1 && sleep 1", "sleep 2 && echo 2"])
         .output()
         .unwrap();
 
     let expected_output = r"[0] 1
-[0] echo 1 exited with code 0
+[0] echo 1 && sleep 1 exited with code 0
 [1] 2
-[1] sleep 1 && echo 2 exited with code 0
+[1] sleep 2 && echo 2 exited with code 0
 ";
     assert_eq!(&String::from_utf8_lossy(&output.stdout), expected_output);
 }
